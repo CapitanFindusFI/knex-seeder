a14423b2fb17a4484cc9e89e95798294224850df
CapitanFindusFI <am95.dev@gmail.com>
2020-02-18 09:59:18 +0100

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import ISeeder from "./interfaces";
+import ISeeder from './interfaces';
 
 class Seeder {
-    public static run<T>(seeder: (new () => ISeeder<T>), howMany: number = 1, attributes: T | null = null): T[] {
-        const seed = new seeder();
-        return Array.from(Array(howMany).keys()).map(() => seed.item(attributes))
-    }
+  public static run<T>(seeder: new () => ISeeder<T>, howMany: number = 1, attributes: T | null = null): T[] {
+    const seed = new seeder();
+    return Array.from(Array(howMany).keys()).map(() => seed.item(attributes));
+  }
 }
 
-export default Seeder
+export default Seeder;
